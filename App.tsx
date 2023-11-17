6d50234911758c44b137a6e920ae6c6f429ef55f
Sena Demirbaş <201307018@kocaeli.edu.tr>
2023-11-17 17:55:29 +0300

    added timer component

--- App.tsx
+++ App.tsx
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { TimerCountDownDisplay } from "./components/TimerCountDownDisplay";
 
+const FOCUS_TIME_MINUTES = 0.2 * 60 * 1000;
+const BREAK_TIME_MINUTES = 0.1 * 60 * 1000;
 export default function App() {
+  const [timerCount, setTimerCount] = useState<number>(FOCUS_TIME_MINUTES);
+  const [timerInterval, setTimerInterval] = useState<NodeJS.Timer | null>(null);
+
+  const startTimer = () => {
+    const id = setInterval(() => setTimerCount((prev) => prev - 1000), 1000);
+    setTimerInterval(id);
+  };
+  const stopTimer = () => {
+    if (timerInterval !== null) {
+      clearInterval(timerInterval as any);
+    }
+  };
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title="Start timer" onPress={startTimer} />
+      <Button title="Stop timer" onPress={stopTimer} />
+      <TimerCountDownDisplay timerDate={new Date(timerCount)} />
     </View>
   );
 }
@@ -13,8 +32,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ styles/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  timerCountDownText: {
+    fontSize: 30,
+    fontWeight: "700",
+  },
+});
+
+export default styles;
